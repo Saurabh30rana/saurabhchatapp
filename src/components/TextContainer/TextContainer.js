7f6427aee677ab8f634380092d87975b25fe1d60
saurabh <saurabhrana0000@gmail.com>
2021-07-07 15:53:30 +0530

    second commit

---
+++ src/components/TextContainer/TextContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users ,user}) => (
+  <div className="textContainer">
+    <div>
+      <h1><strong> Welcome , {user}</strong></h1>
+      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
+      <h2>Designed and developed by SAURABH RANA and KAPIL MEROTHA <span role="img" aria-label="emoji">⬅️</span></h2>
+
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
